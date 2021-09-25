773b2a1b033f84c5cca2c8e23c4fa058d783ae83
NoeCR <noecr.dev@gmail.com>
2021-09-25 10:26:34 +0200

    Initial Commit

---
+++ Shared/Swift_Combine_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Combine_MVVMApp.swift
+//  Shared
+//
+//  Created by Noe Cruz Rodriguez on 25/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Combine_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
